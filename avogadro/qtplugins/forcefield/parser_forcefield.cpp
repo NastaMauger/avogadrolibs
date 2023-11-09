3d85e4892a8793fe2f70df0a4a79d5130ad19567
NastaMauger <nastasia.mauger18@gmail.com>
2023-11-08 19:16:25 -0500

    Clean parser_forcefield.cpp and read close the file in the good way now

--- avogadro/qtplugins/forcefield/parser_forcefield.cpp
+++ avogadro/qtplugins/forcefield/parser_forcefield.cpp
@@ -21,48 +21,48 @@
 namespace Avogadro {
 namespace QtPlugins {
 
-QString ParserForceField::loadAndParseFile(QString& fileName) {
-    fileName = QFileDialog::getOpenFileName(nullptr, "Select Parameter File", QDir::homePath(), "Parameter Files (*.prm *.txt *.out);;All Files (*)");
-    QString fileContent;
 
-    if (!fileName.isEmpty()) {
-        QFile file(fileName);
-        if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-            QTextStream in(&file);
-            while (!in.atEnd()) {
-                QString line = in.readLine();
-                fileContent.append(line);
-                qDebug() << line;
+QString ParserForceField::loadAndParseFile(QString& fileName)
+{
+ fileName = QFileDialog::getOpenFileName(nullptr, "Select Parameter File", QDir::homePath(), "Parameter Files (*.prm *.txt *.out);;All Files (*)");
+ QString fileContent;
 
-                // Check if the line contains the word 'amoeba'
-                if (line.contains("amoeba", Qt::CaseInsensitive)) {
-                    // Found 'amoeba' in the file
-                    QString result = amoebaReadPrm(fileContent);
-                    break;
-                } else {
-                    QMessageBox msgBox;
-                    msgBox.setIcon(QMessageBox::Warning);
-                    msgBox.setText("Only AMOEBA parameter file can be read");
-                    msgBox.setWindowTitle("Error");
-                    msgBox.setStandardButtons(QMessageBox::Ok);
-                    msgBox.exec();
-                    msgBox.close();
-                    file.close();  
-                    return fileContent;
-                }
-            }
-            file.close();
-        } else {
-            // File open error
-            QMessageBox msgBox;
-            msgBox.setIcon(QMessageBox::Critical);
-            msgBox.setText("Error opening the file.");
-            msgBox.setWindowTitle("Error");
-            msgBox.setStandardButtons(QMessageBox::Ok);
-            msgBox.exec();
-        }
+ if (!fileName.isEmpty()) {
+  QFile file(fileName);
+  if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+    QTextStream in(&file);
+    while (!in.atEnd()) {
+      QString line = in.readLine();
+      fileContent.append(line);
     }
-    return fileContent;
+    // Check if the file contains the word 'amoeba'
+    //We might want to add ne readprm function depending on waht people need/want
+    if (fileContent.contains("amoeba", Qt::CaseInsensitive)) {
+      QString result = amoebaReadPrm(fileName);
+    } 
+    else {
+      QMessageBox msgBox;
+      msgBox.setIcon(QMessageBox::Warning);
+      msgBox.setText("Only AMOEBA parameter file can be read");
+      msgBox.setWindowTitle("Error");
+      msgBox.setStandardButtons(QMessageBox::Ok);
+      msgBox.exec();
+      msgBox.close();
+      file.close();
+    }
+  } 
+  else {
+    // File open error
+    QMessageBox msgBox;
+    msgBox.setIcon(QMessageBox::Critical);
+    msgBox.setText("Error opening the file.");
+    msgBox.setWindowTitle("Error");
+    msgBox.setStandardButtons(QMessageBox::Ok);
+    msgBox.exec();
+    file.close();
+  }
+ }
+return fileContent;
 }
 
 
